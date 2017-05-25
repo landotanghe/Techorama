f96a3c752ee5a40d9c4afe4bb19d591c536a976f
Lando Tanghe <lando.tanghe@gmail.com>
2017-05-25 08:24:25 +0200

    Add external module

---
+++ ProfanityPostingModule/ProfanityPostingModule/ProfanityFilterModule.cs
@@ -0,0 +1,21 @@
+﻿using ModuleLibrary;
+using ModuleLibrary.posting;
+
+namespace ProfanityPostingModule
+{
+    public class ProfanityFilterModule : IModule<PreProcessEvent>
+    {
+        private const string FilthyWordReplacement = "%$#@";
+
+        public void Initialize(PreProcessEvent events)
+        {
+            events.ExecutePreprocessing += ReplaceFilthyWords;
+        }
+
+        private void ReplaceFilthyWords(PreprocessArgs args)
+        {
+            var filthyWord = "cheek";
+            args.Input = args.Input.Replace(filthyWord, FilthyWordReplacement);
+        }
+    }
+}
