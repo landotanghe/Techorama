98e9c147e8c9eef52370a01d92735ac6ff872715
Lando Tanghe <lando.tanghe@gmail.com>
2017-05-25 08:43:33 +0200

    add comments

--- Posting/Posting/Program.cs
+++ Posting/Posting/Program.cs
@@ -15,10 +15,14 @@ namespace Posting
             var modules = new List<IModule<PreProcessEvent>>();
             modules.Add(new LoudPostingModule());
 
+            // when the ProfanityPostingModule.dll is in the bin folder, we can access everything that assembly
+            // by using reflection:
+            // ultimately this can come from config files
             var profanityModulePath = "ProfanityPostingModule.ProfanityFilterModule,ProfanityPostingModule";
             var profanityModule = Activator.CreateInstance(Type.GetType(profanityModulePath)) as IModule<PreProcessEvent>;
             modules.Add(profanityModule);
 
+            //only create one PreProcessEvent and then let all the modules register to it.
             var preprocessing = new PreProcessEvent();
             foreach (var module in modules)
             {
@@ -26,8 +30,10 @@ namespace Posting
             }
 
             PreprocessArgs preprocessingArgs = PreProcess(originalInput, preprocessing);
-
+            
             Console.WriteLine($"preprocessed input: {preprocessingArgs.Input}");
+            Console.WriteLine("now we can do some actual processing with this modified input");
+            Console.WriteLine("note: the preprocessing can be simply changed by configuration and without modifying the Posting projects code");
 
             Console.ReadKey();
         }
@@ -45,6 +51,9 @@ namespace Posting
         }
     }
 
+    // example module, the ultimate goal is to be able to create modules 
+    // externally. That way we can plug in functionality without modifying or even rebuilding 
+    // this project. For an example of this, see the ProfanityPostingModule project
     public class LoudPostingModule : IModule<PreProcessEvent>
     {
         public void Initialize(PreProcessEvent events)
