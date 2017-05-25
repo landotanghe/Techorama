02b257880d94307a3f977b2b42626d075ee878eb
Lando Tanghe <lando.tanghe@gmail.com>
2017-05-25 08:13:03 +0200

    Add posting project

---
+++ Posting/Posting/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using ModuleLibrary.posting;
+using ModuleLibrary;
+
+namespace Posting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalInput = "hello world";
+            Console.WriteLine($"originalInput: {originalInput}");
+
+            var modules = new List<IModule<PreProcessEvent>>();
+            modules.Add(new LoudPostingModule());
+
+            var preprocessing = new PreProcessEvent();
+            foreach (var module in modules)
+            {
+                module.Initialize(preprocessing);
+            }
+
+            PreprocessArgs preprocessingArgs = PreProcess(originalInput, preprocessing);
+
+            Console.WriteLine($"preprocessed input: {preprocessingArgs.Input}");
+
+            Console.ReadKey();
+        }
+
+        private static PreprocessArgs PreProcess(string originalInput, PreProcessEvent preprocessing)
+        {
+            var preprocessingArgs = new PreprocessArgs
+            {
+                Input = originalInput
+            };
+
+            preprocessing.ExecutePreprocessing?.Invoke(preprocessingArgs);
+
+            return preprocessingArgs;
+        }
+    }
+
+    public class LoudPostingModule : IModule<PreProcessEvent>
+    {
+        public void Initialize(PreProcessEvent events)
+        {
+            events.ExecutePreprocessing += AddExlamations;
+        }
+
+        private void AddExlamations(PreprocessArgs args)
+        {
+            args.Input = args.Input + "!!!";
+        }
+    }
+}
